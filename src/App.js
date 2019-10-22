a2fca8e52612e3856d22f3145b4849b433831f8e
N4de <jaAnnala@gmail.com>
2019-10-22 17:31:14 +0300

    linting stuff

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import React from 'react';
-
 const App = () => {
-  return(
-    <div>gasgdsa</div>
-  );
+  const [notes, setNotes] = useState([]) 
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+
+  return (
+    <div>
+      <h1>Blogs</h1>
+
+      <Notification message={errorMessage} />
+
+      <h2>Login</h2>
+
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+
+      // ...
+    </div>
+  )
 }
 
-export default App;
+export default App
