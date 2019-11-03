352217bce6f99d8598fa1272a7507dcaf0cc980e
N4de <jaAnnala@gmail.com>
2019-11-03 13:37:01 +0200

    5.11: blogilistan frontend, step11

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const LoginForm = ({
   handleSubmit,
@@ -31,5 +32,11 @@ const LoginForm = ({
   </div>
 );
 
+LoginForm.propTypes = {
+  username: PropTypes.string.isRequired,
+  handleUsernameChange: PropTypes.func.isRequired,
+  password: PropTypes.string.isRequired,
+  handlePasswordChange: PropTypes.func.isRequired,
+};
 
 export default LoginForm;
