d4b0b4f052d4f3f62acc4c4298d153d9081df927
N4de <jaAnnala@gmail.com>
2019-11-09 16:27:17 +0200

    5.14: blogilistan testit, step2

--- src/components/simpleBlog.test.jsx
+++ src/components/simpleBlog.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import SimpleBlog from './simpleBlog';
 
 afterEach(cleanup);
@@ -29,4 +29,24 @@ test('renders content', () => {
   expect(component.container).toHaveTextContent(
     'blog has3likes',
   );
+});
+
+test('button is functional', () => {
+  const blog = {
+    title: 'blogTitle',
+    author: 'blogAuthor',
+    likes: 3,
+  };
+
+  const mockHandler = jest.fn();
+
+  const { getByText } = render(
+    <SimpleBlog blog={blog} onClick={mockHandler} />,
+  );
+
+  const button = getByText('like');
+  fireEvent.click(button);
+  fireEvent.click(button);
+
+  expect(mockHandler.mock.calls.length).toBe(2);
 });
