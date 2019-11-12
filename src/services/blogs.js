c61ce9cc5361d1944909f030228a60b28284b5b4
N4de <jaAnnala@gmail.com>
2019-11-12 22:08:58 +0200

    add forgotten token to stuff so you can make requests

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -2,14 +2,23 @@ import axios from 'axios';
 
 const baseUrl = '/api/blogs';
 
+let token = null;
+
+const setToken = (newToken) => {
+  token = `bearer ${newToken}`;
+};
+
 const getAll = () => {
   const request = axios.get(baseUrl);
   return request.then((response) => response.data);
 };
 
 const createBlog = (newBlog) => {
-  const request = axios.post(baseUrl, newBlog);
+  const config = {
+    headers: { Authorization: token },
+  };
+  const request = axios.post(baseUrl, newBlog, config);
   return request.then((response) => response.data);
 };
 
-export default { getAll, createBlog };
+export default { getAll, createBlog, setToken };
